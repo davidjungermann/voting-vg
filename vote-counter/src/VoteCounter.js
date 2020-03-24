87b67596b9fb5ecb7eaca32d45d6b59cbe81f78f
ravedave <dic15dju@student.lu.se>
2020-03-24 19:46:28 +0100

    git fix

--- vote-counter/src/VoteCounter.js
+++ vote-counter/src/VoteCounter.js
@@ -1,7 +1,8 @@
+const Excel = require('exceljs');
+
 async function readFile() {
-    const Excel = require('exceljs');
-    var workbook = new Excel.Workbook();
-    workbook.xlsx.readFile("../../test.xlsx").then(workbook => {
+    let voting_workbook = new Excel.Workbook();
+    voting_workbook.xlsx.readFile("../../test.xlsx").then(workbook => {
         var test = workbook.getWorksheet()
         console.log(test.getColumn("B").eachCell(content => console.log(content.text)));
     }
