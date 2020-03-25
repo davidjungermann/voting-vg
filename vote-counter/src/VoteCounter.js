c41287122fd2b09efd4a5b96824001b88d27e502
ravedave <dic15dju@student.lu.se>
2020-03-25 10:10:52 +0100

    async up in thiis bi

--- vote-counter/src/VoteCounter.js
+++ vote-counter/src/VoteCounter.js
@@ -1,27 +1,33 @@
 const Excel = require('exceljs');
 
-function compareVotingCodes() {
+async function initVotes() {
     let voting_workbook = new Excel.Workbook();
+    let codes = [];
+
+    const workbook = await voting_workbook.xlsx.readFile("../../test.xlsx");
+    workbook.getWorksheet().getColumn("B").eachCell(content => codes.push(content.text));
+    return codes.slice(1);
+}
+
+async function initReferenceCodes() {
     let reference_workbook = new Excel.Workbook();
-    var codes = [];
-    var referenceCodes = [];
+    let referenceCodes = [];
 
-    voting_workbook.xlsx.readFile("../../test.xlsx").then(workbook => {
-        workbook.getWorksheet().getColumn("B").eachCell(content => codes.push(content.text));
-        codes = codes.slice(1);
-    });
+    const workbook = await reference_workbook.xlsx.readFile("../../voting_codes.xlsx");
+    workbook.getWorksheet().getColumn("B").eachCell(content => referenceCodes.push(content.text));
+    return referenceCodes;
+}
+async function compareVotingCodes() {
+    let codes = await initVotes();
+    let referenceCodes = await initReferenceCodes();
 
-    reference_workbook.xlsx.readFile("../../voting_codes.xlsx").then(workbook => {
-        workbook.getWorksheet().getColumn("B").eachCell(content => referenceCodes.push(content.text));
-    }).then(() => {
-        codes.forEach(code => {
-            if (referenceCodes.includes(code)) {
-                console.log("Valid vote: " + code);
-                referenceCodes = referenceCodes.filter(x => x !== code);
-            } else {
-                console.log("Invalid vote: " + code);
-            }
-        });
+    codes.forEach(code => {
+        if (referenceCodes.includes(code)) {
+            console.log("Valid vote: " + code);
+            referenceCodes = referenceCodes.filter(x => x !== code);
+        } else {
+            console.log("Invalid vote: " + code);
+        }
     });
 }
 compareVotingCodes();
