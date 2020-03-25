dc38df73517cb086476552f2be38f813783ea98f
ravedave <dic15dju@student.lu.se>
2020-03-25 09:35:18 +0100

    can check for votes

--- vote-counter/src/VoteCounter.js
+++ vote-counter/src/VoteCounter.js
@@ -1,11 +1,27 @@
 const Excel = require('exceljs');
 
-async function readFile() {
+function compareVotingCodes() {
     let voting_workbook = new Excel.Workbook();
+    let reference_workbook = new Excel.Workbook();
+    var codes = [];
+    var referenceCodes = [];
+
     voting_workbook.xlsx.readFile("../../test.xlsx").then(workbook => {
-        var test = workbook.getWorksheet()
-        console.log(test.getColumn("B").eachCell(content => console.log(content.text)));
-    }
-    );
+        workbook.getWorksheet().getColumn("B").eachCell(content => codes.push(content.text));
+        codes = codes.slice(1);
+    });
+
+    reference_workbook.xlsx.readFile("../../voting_codes.xlsx").then(workbook => {
+        workbook.getWorksheet().getColumn("B").eachCell(content => referenceCodes.push(content.text));
+    }).then(() => {
+        codes.forEach(code => {
+            if (referenceCodes.includes(code)) {
+                console.log("Valid vote: " + code);
+                referenceCodes = referenceCodes.filter(x => x !== code);
+            } else {
+                console.log("Invalid vote: " + code);
+            }
+        });
+    });
 }
-readFile();
+compareVotingCodes();
