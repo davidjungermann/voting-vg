b23cb7063aa5333217afaa451f3c37ce822dbe09
David Jungermann <dic15dju@student.lu.se>
2020-04-05 11:32:57 +0200

    Removed api keys and config

--- vote-counter/src/FirebaseInstance.js
+++ vote-counter/src/FirebaseInstance.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyCQnGWPb9g0qVM-mYsuvC-MlbWztZBdtmw",
-  authDomain: "vg-voting-83c84.firebaseapp.com",
-  databaseURL: "https://vg-voting-83c84.firebaseio.com",
-  projectId: "vg-voting-83c84",
-  storageBucket: "vg-voting-83c84.appspot.com",
-  messagingSenderId: "14819118797",
-  appId: "1:14819118797:web:6a3006215809c1a8ffe384"
+  // Config not included. 
 };
 
 class FirebaseInstance {
