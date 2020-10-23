45b62977a2ca92f8ba42f766d600d681f5aebbf4
DavidJungermann <dic15dju@student.lu.se>
2020-10-23 12:40:25 +0200

    restruct

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import App from "./App";
-import "./styles.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
