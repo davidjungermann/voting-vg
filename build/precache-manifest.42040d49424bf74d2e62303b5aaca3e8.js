3ad7334522a9c164733f81a6709b599fb0cf9819
DavidJungermann <dic15dju@student.lu.se>
2020-10-24 18:31:03 +0200

    deploy

---
+++ build/precache-manifest.42040d49424bf74d2e62303b5aaca3e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66272780bace65c1abeeb8a928d633ff",
+    "url": "/voting-vg/index.html"
+  },
+  {
+    "revision": "34d6b3fde74fdbe8fbb5",
+    "url": "/voting-vg/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "c2f662b656ecaf527a5d",
+    "url": "/voting-vg/static/css/main.c12befbc.chunk.css"
+  },
+  {
+    "revision": "34d6b3fde74fdbe8fbb5",
+    "url": "/voting-vg/static/js/2.dc9120bc.chunk.js"
+  },
+  {
+    "revision": "1bbbe2c8a142fafecca45007c9f675ca",
+    "url": "/voting-vg/static/js/2.dc9120bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f662b656ecaf527a5d",
+    "url": "/voting-vg/static/js/main.451ced09.chunk.js"
+  },
+  {
+    "revision": "371ed2e432fcffdf1aef",
+    "url": "/voting-vg/static/js/runtime-main.28e234f0.js"
+  }
+]);
