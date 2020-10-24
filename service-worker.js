ccfe06ca15c7360db963695e9b5f200ac80c7865
DavidJungermann <dic15dju@student.lu.se>
2020-10-24 18:30:24 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voting-vg/precache-manifest.10c2d17c4ee28ca34c7b8caea303624e.js"
+  "/voting-vg/precache-manifest.42040d49424bf74d2e62303b5aaca3e8.js"
 );
 
 self.addEventListener('message', (event) => {
